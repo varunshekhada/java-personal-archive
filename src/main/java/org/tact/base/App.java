f682b5979013c4a1062c598d0aa16eb20a2c41c1
varu shekhada <varunshekhada13@gmail.com>
2019-02-01 16:19:25 +0530

    simple comment

--- src/main/java/org/tact/base/App.java
+++ src/main/java/org/tact/base/App.java
@@ -3,6 +3,6 @@ package org.tact.base;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Java Personal Archive");
+		System.out.println("Welcome to Bangalore");
 	}
 }
